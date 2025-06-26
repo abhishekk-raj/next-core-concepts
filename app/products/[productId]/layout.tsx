51e72379beaf50fb60fcfbca48328dc4a87375d4
Abhishek Raj <abhishekk.raj@outlook.com>
2025-06-26 20:20:06 +0530

    Nested Layout

---
+++ app/products/[productId]/layout.tsx
@@ -0,0 +1,10 @@
+const ProductDetailLayout = ({children}: {children: React.ReactNode}) => {
+    return (
+        <>
+            {children}
+            <h2> Featured products </h2>
+        </>
+    )
+}
+
+export default ProductDetailLayout;
