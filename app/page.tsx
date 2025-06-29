fe367b3995de2e91c73e99611aab9824657c140a
Abhishek Raj <abhishekk.raj@outlook.com>
2025-06-29 15:51:48 +0530

    Link component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Home page</h1>
+    <div className="flex flex-col">
+      <h1>Home page</h1>
+      <Link href={'/blog'}>Blog</Link>
+      <Link href={'/products'}>Products</Link>
+    </div>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 const Products = () => {
+    const productId = 100;
+
     return (
         <div>
             <h1> Product List </h1>
-            <h4> Product 1 </h4>
-            <h4> Product 2 </h4>
-            <h4> Product 3 </h4>
-            <h4> Product 4 </h4>
+            <h4> <Link href={'products/1'}>Product 1</Link> </h4>
+            <h4> <Link href={'products/2'}>Product 2</Link> </h4>
+            <h4> <Link href={'products/3'}>Product 3</Link> </h4>
+            <h4> <Link href={'products/4'}>Product 4</Link> </h4>
+            <h4> <Link href={`products/${productId}`}>Product {productId}</Link> </h4>
         </div>
     )
 }
