e0153ca08866fe4224fcf38ab074815990add340
Abhishek Raj <abhishekk.raj@outlook.com>
2025-06-30 20:34:49 +0530

    Recovering from error

--- app/products/[productId]/layout.tsx
+++ app/products/[productId]/layout.tsx
@@ -1,9 +1,9 @@
 const ProductDetailLayout = ({children}: {children: React.ReactNode}) => {
     return (
-        <>
+        <div className="flex flex-col">
             {children}
             <h2> Featured products </h2>
-        </>
+        </div>
     )
 }
 
--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-const ErrorBoundary = ({error}: {error: Error}) => {
-    return <h1 className="text-red-500">{error.message}</h1>
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+const ErrorBoundary = ({error, reset}: {error: Error, reset: () => void}) => {
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        })
+    }
+
+    return <>
+        <h1 className="text-red-500">{error.message}</h1>
+        <button onClick={reload} className="border rounded-2xl">Try Again</button>
+    </>
 }
 
 export default ErrorBoundary;
