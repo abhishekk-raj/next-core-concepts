a87fc3e4f20926239ceede68e39b7d2a161de9cc
Abhishek Raj <abhishekk.raj@outlook.com>
2025-06-30 20:58:20 +0530

    Handling error layout - it will not work

--- app/products/[productId]/layout.tsx
+++ app/products/[productId]/layout.tsx
@@ -1,4 +1,13 @@
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 const ProductDetailLayout = ({children}: {children: React.ReactNode}) => {
+    const random = getRandomInt(2);
+    if(random === 1) {
+        throw new Error("Error loading product");
+    }
+
     return (
         <div className="flex flex-col">
             {children}
